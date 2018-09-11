73156b57a857554bf1cd78cf59b872acaee09dc6
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-09-11 21:01:18 +0300

    Update Begin.java

--- src/Begin.java
+++ src/Begin.java
@@ -1,15 +1,18 @@
-/**
- * Created by Ксения on 11.09.2018.
- */
 public class Begin {
+
+    public class Ncl {
+        public Ncl(String str) {
+
+            System.out.println(str);
+        }
+    }
     public static void main(String[] args) {
 
+        String[] sarray = new String[4];
+        for(int i=0; i<4;i++)
+            sarray[i]="Hey, Jude!";
+        for(int i=0; i<4;i++)
+        System.out.println(sarray[i]);
 
-        String Sarray[];
-        Sarray = new String[4];
-        for(int i=0; i<4;i++)
-            Sarray[i]="Hey, Jude!";
-        for(int i=0; i<4;i++)
-        System.out.println(Sarray[i]);
     }
 }
