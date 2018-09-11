c91ef83cb7191d0b508b87c9ac7ff3481ba4d15c
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-09-11 20:46:38 +0300

    Update Begin.java

--- src/Begin.java
+++ src/Begin.java
@@ -3,6 +3,13 @@
  */
 public class Begin {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        String Sarray[];
+        Sarray = new String[4];
+        for(int i=0; i<4;i++)
+            Sarray[i]="Hey, Jude!";
+        for(int i=0; i<4;i++)
+        System.out.println(Sarray[i]);
     }
 }
