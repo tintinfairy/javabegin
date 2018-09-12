0e316e77fa8d883e37ea79137be31f8578d1d3db
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-09-12 13:18:45 +0300

    Update Begin.java

--- src/Begin.java
+++ src/Begin.java
@@ -1,21 +1,27 @@
 public class Begin {
 
-    public class Ncl {
+    static class Ncl {
         String name;
+
         public Ncl(String str) {
 
-            name=str;
-            System.out.println(str);
+            name = str;
         }
 
-        public void Yourname(String name){System.out.println(name);}
-    }
-        public static void main(String[] args) {
+        public void Yourname() {
+            System.out.println(name);
 
-            String[] sarray = new String[4];
-            for (int i = 0; i < 4; i++)
-                sarray[i] = "Hey, Jude!";
-            for (int i = 0; i < 4; i++)
-                System.out.println(sarray[i]);
+        }
     }
+    public static void main(String[] args) {
+
+        /*String[] sarray = new String[4];
+        for (int i = 0; i < 4; i++)
+            sarray[i] = "Hey, Jude!";
+        for (int i = 0; i < 4; i++)
+            System.out.println(sarray[i]);*/
+        Ncl nlc = new Ncl("Kate");
+        nlc.Yourname();
+    }
+
 }
