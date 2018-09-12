bd352387473490af076a256cc2848699a3984c13
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-09-12 14:05:33 +0300

    Update Begin.java

--- src/Begin.java
+++ src/Begin.java
@@ -1,27 +1,12 @@
 public class Begin {
 
-    static class Ncl {
-        String name;
-
-        public Ncl(String str) {
-
-            name = str;
-        }
-
-        public void Yourname() {
-            System.out.println(name);
-
-        }
-    }
     public static void main(String[] args) {
 
-        /*String[] sarray = new String[4];
-        for (int i = 0; i < 4; i++)
-            sarray[i] = "Hey, Jude!";
-        for (int i = 0; i < 4; i++)
-            System.out.println(sarray[i]);*/
-        Ncl nlc = new Ncl("Kate");
-        nlc.Yourname();
+
+        Textproc nstr = new Textproc("Kate Loy");
+        nstr.Yourname();
+        nstr.Simbnumber();
+        nstr.Words();
     }
 
 }
