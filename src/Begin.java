b7ff62f8a09fefea80205dbaa884c3e055ce30aa
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-09-12 15:17:33 +0300

    Update Begin.java

--- src/Begin.java
+++ src/Begin.java
@@ -1,12 +1,32 @@
+import java.io.*;
+import org.apache.commons.lang.RandomStringUtils;
 public class Begin {
 
-    public static void main(String[] args) {
+    public static String givenUsingApache_whenGeneratingRandomStringBounded_thenCorrect() {
+
+        int length = 50;
+        boolean useLetters = true;
+        boolean useNumbers = false;
+        String generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+
+        return generatedString;
+    }
+
+    //"C:\\Users\\Ксения\\IdeaProjects\\Ex1\\src\\main\\java\\Information.txt"
+    public static void main(String[] args) throws Exception {
 
 
-        Textproc nstr = new Textproc("Kate Loy");
-        nstr.Yourname();
+        Textproc nstr = new Textproc(givenUsingApache_whenGeneratingRandomStringBounded_thenCorrect());
+
+        FileWriter fw = new FileWriter("C:\\Users\\Ксения\\IdeaProjects\\Ex1\\src\\main\\java\\Information.txt");
+        fw.write(nstr.Yourname()+" "+nstr.Simbnumber());
+        fw.close();
+
+
+       /* nstr.Yourname();
         nstr.Simbnumber();
-        nstr.Words();
+        nstr.Words();*/
+
     }
 
 }
