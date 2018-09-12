9abeecd5288982413913db389e2124b0846292cd
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-09-12 16:33:42 +0300

    Update Begin.java

--- src/Begin.java
+++ src/Begin.java
@@ -1,14 +1,25 @@
 import java.io.*;
 import org.apache.commons.lang.RandomStringUtils;
+import java.util.Random;
 public class Begin {
 
     public static String givenUsingApache_whenGeneratingRandomStringBounded_thenCorrect() {
 
-        int length = 50;
+        Random rnd = new Random(System.currentTimeMillis());
+        int min=1;
+        int length = min + rnd.nextInt(10);
+        int n = min + rnd.nextInt(10);
         boolean useLetters = true;
         boolean useNumbers = false;
-        String generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+        String generatedString="" ;
+        String generatedString1 = RandomStringUtils.random(length, useLetters, useNumbers);
 
+        for(int i=0; i<n; i++) {
+            length = min + rnd.nextInt(10);
+            generatedString = generatedString + " " + generatedString1;
+            generatedString1 = RandomStringUtils.random(length, useLetters, useNumbers);
+
+        }
         return generatedString;
     }
 
@@ -18,10 +29,11 @@ public class Begin {
         Textproc nstr = new Textproc(givenUsingApache_whenGeneratingRandomStringBounded_thenCorrect());
 
         FileWriter fw = new FileWriter("C:\\Users\\Ксения\\IdeaProjects\\Ex1\\src\\main\\java\\Information.txt");
-        fw.write(nstr.Yourname()+" "+nstr.Simbnumber());
+        fw.write(nstr.Yourname()+" "+nstr.Simbnumber(nstr.Words()));
         for (String Substr:nstr.Words()) fw.write(" "+Substr);
         fw.close();
 
     }
 
 }
+
