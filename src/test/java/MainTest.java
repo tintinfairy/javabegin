28111482cef433690b4b6c84039fad1f0a23db90
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-09-13 20:37:25 +0300

    Add files via upload

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowException() {
+        TextProc.createRandomText(0, 10, 1, 10);
+
+    }
+
+    @Test
+    public void shouldCompareAmount() {
+
+        TextProc nInst = new TextProc("Hale Hell");
+        assertEquals(2, nInst.wordsAmount());
+
+
+    }
+
+    @Test
+    public void shouldTestTextEquality() {
+
+        TextProc nInst = new TextProc("Hale Hell");
+        assertEquals("Hall Hell", nInst.getText());
+
+
+    }
+
+}
+
