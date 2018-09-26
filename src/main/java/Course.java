57aa4df614e30fcb2a694d223f11ec616d4452d5
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-09-26 19:19:55 +0300

    Add files via upload

---
+++ src/main/java/Course.java
@@ -0,0 +1,9 @@
+public class Course {
+    private String title;
+    private String desription;
+
+    public Course(String title, String desription) {
+        this.title = title;
+        this.desription = desription;
+    }
+}
