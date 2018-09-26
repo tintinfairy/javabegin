e6b9b6e5ee2bbdd55cda89a6a1e8a18dfc931c90
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-09-26 19:22:05 +0300

    last version of UrlDownloader+json

--- src/main/java/UrlDownloader.java
+++ src/main/java/UrlDownloader.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.io.IOException;
 
 import org.jsoup.Jsoup;
@@ -10,17 +11,22 @@ public class UrlDownloader {
 
     public static void main(String[] args) throws IOException {
 
-        String url = "https://geekbrains.ru/courses/3";
+        String url = "https://geekbrains.ru/courses/4";
         Document doc = Jsoup.connect(url).get();
 
         // gettig title
         String title = doc.title();
         System.out.println("Title: " + title);
+        System.out.println();
 
         //getting description
         String description = doc.select("meta[name=description]").first().attr("content");
         System.out.println("Description : " + description);
+        Writer writer = new FileWriter("Output.json");
+        Gson gson = new GsonBuilder().create();
+        gson.toJson(new Course(title, description), writer);
 
+        writer.close();
     }
 
 }
