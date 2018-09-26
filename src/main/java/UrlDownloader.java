5bba0a2f4e4ba2d438e954cdaad2eea37bd1e9b8
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-09-26 22:09:44 +0300

    UrlDownloader with changing ID

--- src/main/java/UrlDownloader.java
+++ src/main/java/UrlDownloader.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.io.IOException;
 
 import org.jsoup.Jsoup;
+import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -11,21 +12,34 @@ public class UrlDownloader {
 
     public static void main(String[] args) throws IOException {
 
-        String url = "https://geekbrains.ru/courses/4";
-        Document doc = Jsoup.connect(url).get();
-
-        // getting title
-        String title = doc.title();
-        System.out.println("Title: " + title);
-        System.out.println();
-
-        //getting description
-        String description = doc.select("meta[name=description]").first().attr("content");
-        System.out.println("Description : " + description);
+        int id = 1;
         Writer writer = new FileWriter("Output.json");
-        Gson gson = new GsonBuilder().create();
-        gson.toJson(new Course(title, description), writer);
 
+
+        while (id <= 200) {
+
+            String url = "https://geekbrains.ru/courses/" + Integer.toString(id);
+            Connection.Response response = Jsoup.connect(url).followRedirects(false).execute();
+            if (response.statusCode() != 302) {
+
+                System.out.println("https://geekbrains.ru/courses/" + id);
+                Document doc = Jsoup.connect(url).get();
+
+                // getting title
+                String title = doc.title();
+                System.out.println("Title:" + title);
+
+                //getting description
+                String description = doc.select("meta[name=description]").first().attr("content");
+                System.out.println("Description:" + description);
+
+                Gson gson = new GsonBuilder().create();
+                gson.toJson(new Course(title, description), writer);
+            }
+
+
+            id++;
+        }
         writer.close();
     }
 
