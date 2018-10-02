9d4780edc76e547bdc6fac7ba1e2c501ba690f62
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-10-03 00:42:03 +0300

    Update Course.javaa

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,15 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Course {
     private String title;
     private String description;
     private String[] words;
-    private int uniwords;
+    private Map<String, Integer> uniwords = new HashMap<String, Integer>();
     TextProc textProc = new TextProc();
 
     public Course(String title, String description) {
         this.title = title;
         this.description = description;
         this.words = this.description.split("\\s");
-        this.uniwords = textProc.wordsUniqueness(this.words);
+        this.uniwords = textProc.getHashmap();
     }
 
 }
