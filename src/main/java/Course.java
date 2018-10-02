e26ae13c8177dc5407a742a88b6a32beeaa74731
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-10-02 22:35:08 +0300

    Update Course.java

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,7 +1,3 @@
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.io.*;
-
 public class Course {
     private String title;
     private String description;
@@ -16,49 +12,5 @@ public class Course {
         this.uniwords = textProc.wordsUniqueness(this.words);
     }
 
-    public static String executePost(String targetURL, String urlParameters) {
-        HttpURLConnection connection = null;
-
-        try {
-            //Create connection
-            URL url = new URL(targetURL);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type",
-                    "application/x-www-form-urlencoded");
-
-            connection.setRequestProperty("Content-Length",
-                    Integer.toString(urlParameters.getBytes().length));
-            connection.setRequestProperty("Content-Language", "en-US");
-
-            connection.setUseCaches(false);
-            connection.setDoOutput(true);
-
-            //Send request
-            DataOutputStream wr = new DataOutputStream (
-                    connection.getOutputStream());
-            wr.writeBytes(urlParameters);
-            wr.close();
-
-            //Get Response
-            InputStream is = connection.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
-            String line;
-            while ((line = rd.readLine()) != null) {
-                response.append(line);
-                response.append('\r');
-            }
-            rd.close();
-            return response.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-    }
 }
 
