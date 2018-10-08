74cbabc61018232f3afc05b476a78459d9328376
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-10-08 11:46:09 +0300

    Update TextProc.java

--- src/main/java/TextProc.java
+++ src/main/java/TextProc.java
@@ -18,17 +18,13 @@ public class TextProc {
 
     public Map<String, Integer> getHashmap() {
         String[] words = this.words();
-        int count = 0;
         for (String w : words) {
             Integer i = hashmap.get(w);
             if (i == null)
                 hashmap.put(w, 1);
             else hashmap.put(w, i + 1);
         }
-        for (String w : words) {
-            if (hashmap.get(w) == 1) count++;
-        }
+
         return hashmap;
     }
 }
-
