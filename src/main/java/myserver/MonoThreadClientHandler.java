dabbf4b8df514b530e5355f71d2297ece569c5ed
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-10-17 20:35:11 +0300

    Update MonoThreadClientHandler.java

--- src/main/java/myserver/MonoThreadClientHandler.java
+++ src/main/java/myserver/MonoThreadClientHandler.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.net.Socket;
 import java.io.EOFException;
 
-public class MonoThreadClientHandler implements Runnable {
+public class MonoThreadClientHandler implements Runnable, ServerComands{
 
     private static Socket clientDialog;
 
@@ -17,27 +17,15 @@ public class MonoThreadClientHandler implements Runnable {
     public void run() {
 
         try {
-            // инициируем каналы общения в сокете, для сервера
 
-            // канал записи в сокет следует инициализировать сначала канал чтения для избежания блокировки выполнения программы на ожидании заголовка в сокете
             DataOutputStream out = new DataOutputStream(clientDialog.getOutputStream());
-
-// канал чтения из сокета
             DataInputStream in = new DataInputStream(clientDialog.getInputStream());
             System.out.println("DataInputStream created");
-
             System.out.println("DataOutputStream  created");
-            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            // основная рабочая часть //
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            // начинаем диалог с подключенным клиентом в цикле, пока сокет не
-            // закрыт клиентом
             while (!clientDialog.isClosed()) {
                 System.out.println("Server reading from channel");
 
-                // серверная нить ждёт в канале чтения (inputstream) получения
-                // данных клиента после получения данных считывает их
                 String entry;
                 try {
                     entry = in.readUTF();
@@ -45,61 +33,49 @@ public class MonoThreadClientHandler implements Runnable {
                     break;
                 }
 
-                // и выводит в консоль
                 System.out.println("READ from clientDialog message - " + entry);
 
-                // инициализация проверки условия продолжения работы с клиентом
-                // по этому сокету по кодовому слову - quit в любом регистре
 
                 switch (entry) {
                     case "quit":
                         System.out.println("Client said QUIT");
                         out.writeUTF("Server reply - " + entry + "-OK");
-                        Thread.sleep(3000);
                         clientDialog.close();
                         break;
                     case "Hello, server":
 
                         out.writeUTF("Hello, client");
-                        Thread.sleep(3000);
                         break;
                     default:
                         System.out.println("Server try writing to channel");
                         out.writeUTF("Server reply - " + entry + "-OK");
                 }
 
-                // если условие окончания работы не верно - продолжаем работу -
-                // отправляем эхо обратно клиенту
+
 
                 System.out.println("Server Wrote message to clientDialog.");
 
-                // освобождаем буфер сетевых сообщений
                 out.flush();
 
-                // возвращаемся в началло для считывания нового сообщения
             }
 
-            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            // основная рабочая часть //
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            // если условие выхода - верно выключаем соединения
             System.out.println("Client disconnected");
             System.out.println("Closing connections & channels.");
 
-            // закрываем сначала каналы сокета !
+
             in.close();
             out.close();
 
-            // потом закрываем сокет общения с клиентом в нити моносервера
+
             clientDialog.close();
 
             System.out.println("Closing connections & channels - DONE.");
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
+        } /*catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }
+        }*/
     }
 }
