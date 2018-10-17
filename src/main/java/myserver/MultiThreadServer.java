b0f65364903741c19b3dbb95debe932fa8bc81f7
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-10-17 20:36:08 +0300

    Update MultiThreadServer.java

--- src/main/java/myserver/MultiThreadServer.java
+++ src/main/java/myserver/MultiThreadServer.java
@@ -1,5 +1,4 @@
 package myserver;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,18 +7,23 @@ import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-/**
- * @author mercenery
- */
-public class MultiThreadServer extends Thread {
+public class MultiThreadServer extends Thread implements ServerComands{
 
     static ExecutorService executeIt = Executors.newFixedThreadPool(2);
 
+    ServerSocket server;
+    public MultiThreadServer(){
+        try{
+        server = new ServerSocket(3345);}
+        catch (IOException e) {
+            e.printStackTrace();}
+
+    }
 
     public void run() {
 
         // стартуем сервер на порту 3345 и инициализируем переменную для обработки консольных команд с самого сервера
-        try (ServerSocket server = new ServerSocket(3345);
+        try (
              BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.println("Server socket created, command console reader for listen to server commands");
 
