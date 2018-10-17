c0c9a5f21fb89372ac33a5bab8473fd1f326be5c
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-10-17 20:29:45 +0300

    Update ClientTester.java

--- src/test/java/ClientTester.java
+++ src/test/java/ClientTester.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 import myserver.MultiThreadServer;
 
 import java.io.DataInputStream;
@@ -10,33 +12,24 @@ import java.net.Socket;
 
 public class ClientTester {
 
-    static Socket socket;
-
-    public ClientTester() {
-        try {
-            ( new Thread(new MultiThreadServer())).start();
-            socket = new Socket("localhost", 3345);
-            System.out.println("Client connected to socket");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
     @Test
     public void run() {
 
-        try (
 
-                DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
-                DataInputStream ois = new DataInputStream(socket.getInputStream())) {
+        try (Socket socket = new Socket("localhost", 3345);
+             DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
+             DataInputStream ois = new DataInputStream(socket.getInputStream());){
+            new Thread(new MultiThreadServer()).start();
+
+
             System.out.println("Client oos & ois initialized");
             oos.writeUTF("Hello, server");
             oos.flush();
             System.out.println("Client wrote & start waiting for data from server...");
             System.out.println("reading...");
             String in = ois.readUTF();
-            ////
-            assertEquals("Server should reply correct hello message","Hello, client", in);
-            /////
+
+            assertEquals("Server should reply correct hello message", "Hello, client", in);
             System.out.println(in);
 
             oos.writeUTF("quit");
@@ -46,15 +39,14 @@ public class ClientTester {
             String inn = ois.readUTF();
             System.out.println(inn);
 
-
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        } catch (InterruptedException e) {
+        } /*catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }
+        }*/
 
     }
 
-}
+
