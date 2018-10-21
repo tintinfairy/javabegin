d780852cc4122f1017725ccde3483a73038373c4
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-10-21 11:39:05 +0300

    Udate MultiThreadServer.java
    Trash comments were deleted + Logging was added

--- src/main/java/myserver/MultiThreadServer.java
+++ src/main/java/myserver/MultiThreadServer.java
@@ -1,5 +1,7 @@
 package myserver;
+
 import java.io.BufferedReader;
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
@@ -7,62 +9,64 @@ import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MultiThreadServer extends Thread implements ServerComands{
+
+public class MultiThreadServer extends Thread implements Closeable  {
 
     static ExecutorService executeIt = Executors.newFixedThreadPool(2);
+    boolean isClosed = false;
 
-    ServerSocket server;
-    public MultiThreadServer(){
-        try{
-        server = new ServerSocket(3345);}
-        catch (IOException e) {
-            e.printStackTrace();}
 
+    Log my_logger = new Log("log.txt");
+    private ServerSocket server;
+
+    public MultiThreadServer() throws IOException {
+        try {
+
+            server = new ServerSocket(3345);
+            my_logger.logger.info("Server started");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+
     public void run() {
-
-        // стартуем сервер на порту 3345 и инициализируем переменную для обработки консольных команд с самого сервера
         try (
-             BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.println("Server socket created, command console reader for listen to server commands");
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
-            // стартуем цикл при условии что серверный сокет не закрыт
+            my_logger.logger.info("Server socket created, command console reader for listen to server commands");
+
             while (!server.isClosed()) {
 
-                // проверяем поступившие комманды из консоли сервера если такие
-                // были
                 if (br.ready()) {
-                    System.out.println("Main Server found any messages in channel, let's look at them.");
 
-                    // если команда - quit то инициализируем закрытие сервера и
-                    // выход из цикла раздачии нитей монопоточных серверов
                     String serverCommand = br.readLine();
                     if (serverCommand.equalsIgnoreCase("quit")) {
-                        System.out.println("Main Server initiate exiting...");
+                        my_logger.logger.info("Main Server initiate exiting...");
                         server.close();
                         break;
                     }
                 }
 
-                // если комманд от сервера нет то становимся в ожидание
-                // подключения к сокету общения под именем - "clientDialog" на
-                // серверной стороне
+
                 Socket client = server.accept();
 
-                // после получения запроса на подключение сервер создаёт сокет
-                // для общения с клиентом и отправляет его в отдельную нить
-                // в Runnable(при необходимости можно создать Callable)
-                // монопоточную нить = сервер - MonoThreadClientHandler и тот
-                // продолжает общение от лица сервера
                 executeIt.execute(new MonoThreadClientHandler(client));
-                System.out.print("Connection accepted.");
+                my_logger.logger.info("Connection accepted.");
             }
 
-            // закрытие пула нитей после завершения работы всех нитей
             executeIt.shutdown();
         } catch (IOException e) {
-            e.printStackTrace();
+            if (!isClosed) e.printStackTrace();
+
         }
+    }
+
+    @Override
+    public void close() throws IOException {
+
+        server.close();
+        isClosed = true;
+        my_logger.logger.info("Server is closed.");
     }
 }
