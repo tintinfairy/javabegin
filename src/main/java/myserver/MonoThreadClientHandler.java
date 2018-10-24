aed13bef033dde7acbc75194aa527851377fea57
Kseniya Kudrinskaya <36051301+tintinfairy@users.noreply.github.com>
2018-10-25 00:12:20 +0300

    Update MonoThreadClientHandler.java
    Backend, which connects with storage and frontend.

--- src/main/java/myserver/MonoThreadClientHandler.java
+++ src/main/java/myserver/MonoThreadClientHandler.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 import java.io.EOFException;
 import java.util.Objects;
 
-import org.json.simple.JSONArray;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -27,8 +27,10 @@ public class MonoThreadClientHandler implements Runnable {
 
         try {
 
-            DataOutputStream out = new DataOutputStream(clientDialog.getOutputStream());
-            DataInputStream in = new DataInputStream(clientDialog.getInputStream());
+
+            DataOutputStream outFrontend = new DataOutputStream(clientDialog.getOutputStream());
+            DataInputStream inFrontend = new DataInputStream(clientDialog.getInputStream());
+
             my_logger.logger.info("DataInputStream created");
             my_logger.logger.info("DataOutputStream  created");
             JSONParser parser = new JSONParser();
@@ -40,7 +42,7 @@ public class MonoThreadClientHandler implements Runnable {
 
                 String entry;
                 try {
-                    entry = in.readUTF();
+                    entry = inFrontend.readUTF();
 
                 } catch (EOFException e) {
                     break;
@@ -57,19 +59,50 @@ public class MonoThreadClientHandler implements Runnable {
 
                 if (Objects.equals(cmd, "get_all_courses")) {
 
-                    String result = "{\"cmd\":\"get_all_courses\",\"user_id\":321123,\"body\":{\"page\":0,\"courses\":[{\"title\":\"course1\",\"description\":\"description1\"},{\"title\":\"course2\",\"description\":\"description2\"}]}}";
-                    out.writeUTF(result);
+                    Socket storage = new Socket("localhost", 3333);
+                    DataOutputStream outStorage = new DataOutputStream(storage.getOutputStream());
+                    DataInputStream inStorage = new DataInputStream(storage.getInputStream());
+
+                    outStorage.writeUTF("{\"cmd\":\"get_all_courses\",\"body\":{}}");
+                    outStorage.flush();
+                    String result = inStorage.readUTF();
+
+                    if (result != null) outStorage.writeUTF("{\"cmd\":\"close\",\"body\":{}}");
+                    outStorage.flush();
+
+
+                    outFrontend.writeUTF(result);
+
+                    entry = inStorage.readUTF();
+                    obj = parser.parse(entry);
+                    jsonObject = (JSONObject) obj;
+                    cmd = (String) jsonObject.get("cmd");
+                    if (Objects.equals(cmd, "close")) {
+                        outStorage.writeUTF("Socket is closed!");
+                        storage.close();
+                    }
+
+
+                    entry = inFrontend.readUTF();
+                    obj = parser.parse(entry);
+                    jsonObject = (JSONObject) obj;
+                    cmd = (String) jsonObject.get("cmd");
+                    if (Objects.equals(cmd, "close")) {
+                        outFrontend.writeUTF("{\"cmd\":\"close\",\"body\":{}}");
+                        clientDialog.close();
+                    }
+
 
                 } else {
 
                     my_logger.logger.info("Server try writing to channel");
-                    out.writeUTF("Server reply - " + entry + "-OK");
+                    outFrontend.writeUTF("Server reply - " + entry + "-OK");
                 }
 
 
                 my_logger.logger.info("Server Wrote message to clientDialog.");
 
-                out.flush();
+                outFrontend.flush();
 
             }
 
@@ -78,8 +111,8 @@ public class MonoThreadClientHandler implements Runnable {
             my_logger.logger.info("Closing connections & channels.");
 
 
-            in.close();
-            out.close();
+            inFrontend.close();
+            outFrontend.close();
 
 
             clientDialog.close();
